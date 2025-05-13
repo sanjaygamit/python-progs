8743e6c3ab524a4280ae9e30379857ac1dcc222c
sanjay gamit <sanjugamit@gmail.com>
2025-05-13 12:41:29 +0530

    added sumvaluearray.py

---
+++ sumvaluearray.py
@@ -0,0 +1,20 @@
+#Find out pairs with given sum value of an array
+
+def twosum(arr, sum):
+    arr.sort()
+    left = 0
+    right = len(arr) - 1
+    while(left<=right):
+        if(arr[left]+arr[right]>sum):
+            right = right - 1
+        elif(arr[left]+arr[right]<sum):
+            left = left + 1         
+        elif(arr[left]+arr[right]==sum):
+            print("Values of pair are ",arr[left],"&",arr[right])
+            right = right -1
+            left = left + 1
+
+arr = [5,7,4,3,9,8,19,21]
+sum = 17
+twosum(arr,sum)
+               
