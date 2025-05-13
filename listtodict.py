807c76262a5bffce60b1c05ef5ea76f9dd91f207
sanjay gamit <sanjugamit@gmail.com>
2025-05-13 10:53:20 +0530

    added listtodict

--- listtodict.py
+++ listtodict.py
@@ -1,2 +1,9 @@
 # Write a python program to convert two lists into a dictionary. 
 
+def list_to_dict():
+    keys = [1,2,3]
+    values = ["one", "two", "three"]
+    result = dict(zip(keys,values))
+    print(result)
+
+list_to_dict()
