304f4841ccd21018b72c42d23b4cd5a04ac0cfe2
sanjay gamit <sanjugamit@gmail.com>
2025-05-13 11:03:20 +0530

    added listtodict

--- listtodict.py
+++ listtodict.py
@@ -6,4 +6,13 @@ def list_to_dict():
     result = dict(zip(keys,values))
     print(result)
 
+def dict_to_touple():
+    x = {1: "one", 2: "two", 3: "three"}
+    #result = tuple(x.items())
+    for i in x.items():
+        print(i)
+
+
+
 list_to_dict()
+dict_to_touple()
