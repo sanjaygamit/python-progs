54fb738e58bd9227b39e35cdf396544221d15eb4
sanjay gamit <sanjugamit@gmail.com>
2025-05-13 10:25:28 +0530

    file added frequencyofwords

--- frequencyofwords.py
+++ frequencyofwords.py
@@ -5,10 +5,13 @@
 def word_frequency():
     str = input("Enter a string: ")
     li = str.split()
+    print(li)
     d = {}
     for i in li:
+        print("List value",i)
         if i not in d.keys():
             d[i] = 0
+        print("dictionary value",d[i])    
         d[i]=d[i]+1 
     print(d) 
 
