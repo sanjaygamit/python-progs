5dbb7ea495d26c9d2424c66a1b4082fcb833c7ab
sanjay gamit <sanjugamit@gmail.com>
2025-05-13 10:01:40 +0530

    file added frequencyofwords

--- frequencyofwords.py
+++ frequencyofwords.py
@@ -1,3 +1,15 @@
 # Write a python program to count the frequency of words appearing in a string. 
 
 # eg: Sheena loves eating apple and mango. Her sister also loves eating apple and mango. 
+
+def word_frequency():
+    str = input("Enter a string: ")
+    li = str.split()
+    d = {}
+    for i in li:
+        if i not in d.keys():
+            d[i] = 0
+        d[i]=d[i]+1 
+    print(d) 
+
+word_frequency()          
