44a93b65278264992d2ae7e165d94af4405b07ac
sanjay gamit <sanjugamit@gmail.com>
2025-06-07 13:04:06 +0530

    modified frequencyofwords.py

--- frequencyofwords.py
+++ frequencyofwords.py
@@ -2,17 +2,37 @@
 
 # eg: Sheena loves eating apple and mango. Her sister also loves eating apple and mango, loves loves loves. 
 
+# def word_frequency():
+#     str = input("Enter a string: ")
+#     li = str.split()
+#     # print(li)
+#     d = {}
+#     for i in li:
+#         # print("List value",i)
+#         if i not in d.keys():
+#             d[i] = 0
+#         # print("dictionary value",d[i])    
+#         d[i]=d[i]+1 
+#     print(d) 
+
+# word_frequency()          
+
+
+
+
+
+# I my name is Sanjay, I am 38 and I am a Software Engineer. I am from India. I love coding and I love Python.
+
 def word_frequency():
-    str = input("Enter a string: ")
+    str = input("Enter a string : ")
     li = str.split()
-    # print(li)
     d = {}
-    for i in li:
-        # print("List value",i)
+    for  i in li:
         if i not in d.keys():
             d[i] = 0
-        # print("dictionary value",d[i])    
-        d[i]=d[i]+1 
-    print(d) 
+        d[i] = d[i] + 1
+    print(d)
+if __name__ == "__main__":
+    word_frequency()            
 
-word_frequency()          
+
