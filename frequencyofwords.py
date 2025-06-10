cdc59c64cbc2eec7170d9c13b2670d790dbd4212
sanjay gamit <sanjugamit@gmail.com>
2025-06-10 18:06:56 +0530

    file modified frequencyofwords.py

--- frequencyofwords.py
+++ frequencyofwords.py
@@ -23,16 +23,26 @@
 
 # I my name is Sanjay, I am 38 and I am a Software Engineer. I am from India. I love coding and I love Python.
 
-def word_frequency():
-    str = input("Enter a string : ")
-    li = str.split()
-    d = {}
-    for  i in li:
-        if i not in d.keys():
-            d[i] = 0
-        d[i] = d[i] + 1
-    print(d)
-if __name__ == "__main__":
-    word_frequency()            
+# def word_frequency():
+#     str = input("Enter a string : ")
+#     li = str.split()
+#     d = {}
+#     for  i in li:
+#         if i not in d.keys():
+#             d[i] = 0
+#         d[i] = d[i] + 1
+#     print(d)
+# if __name__ == "__main__":
+#     word_frequency()            
 
+# Write a python program to convert two lists into a dictionary. 
+
+def fn_to_dict():
+    key = [1,2,3]
+    value = ["one", "two", "three"]
+    result = dict(zip(key,value))
+    print(result)
+
+
+fn_to_dict()    
 
