5c4a18947c593609c895c7830e620aa5324e6eba
sanjay gamit <sanjugamit@gmail.com>
2025-06-20 21:44:10 +0530

    file modified integerToDecimal.py

--- integerToDecimal.py
+++ integerToDecimal.py
@@ -14,6 +14,17 @@
 
 # Reversing a String using an Extended Slicing Technique. 
 
-string = 'Python programming'
-a = string[::-1]
-print(a)
+# string = 'Python programming'
+# a = string[::-1]
+# print(a)
+
+# Counting VOWELS  in a given word 
+vowel = ['a','e','i','o','u']
+word = "programming"
+count = 0 
+for i in word:
+    if i  in vowel: 
+        count +=1
+print(count) 
+
+
