92b0b3cedab048b1732ab4f69ddd14208b684fc3
sanjay gamit <sanjugamit@gmail.com>
2025-06-20 21:36:01 +0530

    integerToDecimal.py

--- integerToDecimal.py
+++ integerToDecimal.py
@@ -1,13 +1,19 @@
 # Converting an Integer into Decimal
 
-import decimal
-integer = 111
-print(decimal.Decimal(integer))
-print(type(decimal.Decimal(integer)))
+# import decimal
+# integer = 111
+# print(decimal.Decimal(integer))
+# print(type(decimal.Decimal(integer)))
 
 # Converting an String of Integers into Decimals 
 
-string = '12345'
-a = decimal.Decimal(string)
+# string = '12345'
+# a = decimal.Decimal(string)
+# print(a)
+# print(type(a))
+
+# Reversing a String using an Extended Slicing Technique. 
+
+string = 'Python programming'
+a = string[::-1]
 print(a)
-print(type(a))
