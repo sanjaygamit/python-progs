f1779c3bbe5f41bf3d46f0dec81c159a7161de83
sanjay gamit <sanjugamit@gmail.com>
2025-06-20 21:46:34 +0530

    file modified integerToDecimal.py

--- integerToDecimal.py
+++ integerToDecimal.py
@@ -27,4 +27,13 @@ for i in word:
         count +=1
 print(count) 
 
+# Counting CONSONANTS  in a given word. 
+vowel = ['a','e','i','o','u']
+word = "programming"
+count = 0 
+for i  in word:
+    if i not in vowel: 
+        count +=1
+print(count) 
+
 
